d5de99c0fa20158013b1d9f6f717fa6f6a775d1c
Nithyashree D <nithyashreed777@gmail.com>
2026-02-20 10:16:24 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_QgADTpT61', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '783h1ukkmj21rvo0cu8radjgpf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_uee6aSr3Q', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2marhsld6am71mfq8qcfagk32i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://jdw8wwhjwl.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
